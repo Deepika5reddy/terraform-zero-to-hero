ee599ef429a0c112364a9637dc2a691489e7398d
Deepika5reddy <reddy5deeps@gmail.com>
2024-08-12 16:10:14 -0500

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,10 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "terraform" {
+    ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-042df97c6f0bca42a"
+    key_name = "aws-terraform"
+    
 }
